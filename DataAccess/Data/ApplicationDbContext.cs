1642b10f6d24f27d96d1ef11d2f184f05ee872c4
Chuks Enedeh <c.enedeh@televic.com>
2022-10-21 23:25:22 +0100

    Add project files.

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    {
+        
+    }
+
+    public DbSet<CoffeeShop> CoffeeShops { get; set; }
+}
